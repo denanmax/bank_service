b7dc6cabcc100650edbb28859a74adcd5e5829a7
denanmax <deshis93@gmail.com>
2023-05-05 21:27:16 +0500

    os.path added

--- main/main.py
+++ main/main.py
@@ -1,14 +1,15 @@
 import json
 from utils.load_data import read_data
 from utils.functions import get_last_5_operations
-from utils.fuctionns_to_print import print_date_description
-from utils.fuctionns_to_print import print_amount
-from utils.fuctionns_to_print import print_masked_accounts
+from utils.fuctions_to_print import print_amount
+from utils.fuctions_to_print import print_masked_accounts
+from utils.fuctions_to_print import print_date_description
 
 def main():
     """Читаем наш файл и выводим 5 последних операций"""
+    path = '../operations/operations.json'
     try:
-        data = read_data()
+        data = read_data(path)
         for operation in get_last_5_operations(data):
             print_date_description(operation)
             print_masked_accounts(operation)
