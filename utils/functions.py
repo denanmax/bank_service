f9823cbdd55b8fb1e973078c9f167cdf351e584d
denanmax <deshis93@gmail.com>
2023-05-05 20:15:44 +0500

    some fuctions changed and transfered to other file

--- utils/functions.py
+++ utils/functions.py
@@ -18,7 +18,7 @@ def mask_account(account):
     if not "Счет" in account:
         result = account[:-12] + " " + account[-12:-10] + "** ****" + " " + account[-4:]
     else:
-        result = "**" + account[-4:]
+        result = "Счет: **" + account[-4:]
 
     return result
 
@@ -27,22 +27,21 @@ def convert_date(date_str):
     date_obj = datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S.%f")
     return date_obj.strftime("%d.%m.%Y")
 
-def print_operation(operation):
-    """Собираем информацию из файла и функций и выводим на экран"""
-    formatted_date = convert_date(operation["date"]) # дата нужного формата
-    description = operation["description"] # описание операции
+def print_date_description(operation):
+    """Собираем вместе дату и описание платежа"""
+    print(f"{convert_date(operation['date'])} {operation['description']}")
+
+def print_masked_accounts(operation):
+    """Формируем скрыте данные карты/счета"""
     try:
         from_account = operation["from"] # прорабатываем если "from" отсутствует
     except:
         from_account = ""
     to_account = operation["to"]
-    amount = float(operation["operationAmount"]["amount"]) # сумма операции
-    currency = operation["operationAmount"]["currency"]["name"] # валюта операции
-
-    print(f"{formatted_date} {description}")
     print(f"{mask_account(from_account)} -> {mask_account(to_account)}")
-    print(f"{amount} {currency}")
-    print()
 
+def print_amount(operation):
+    """Формируем сумму и валюту платежа"""
+    print(f"{float(operation['operationAmount']['amount'])} {operation['operationAmount']['currency']['name']}")
 
 
